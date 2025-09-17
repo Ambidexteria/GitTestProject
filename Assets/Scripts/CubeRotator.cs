1536f35cc160b99d4d18dab426bb5d0ef23ac560
Ambidexteria <43927380+Ambidexteria@users.noreply.github.com>
2025-09-17 14:28:48 +0300

    Update CubeRotator.cs

--- Assets/Scripts/CubeRotator.cs
+++ Assets/Scripts/CubeRotator.cs
@@ -4,9 +4,11 @@ public class CubeRotator : MonoBehaviour
 {
     [SerializeField] private Transform _cubeTransform;
     [SerializeField] private float _angularSpeed = 90f;
+    [SerializeField] private bool _enableRotation = false;
 
     private void Update()
     {
-        _cubeTransform.Rotate(Vector3.up, _angularSpeed *  Time.deltaTime);
+        if (_enableRotation)
+            _cubeTransform.Rotate(Vector3.up, _angularSpeed * Time.deltaTime);
     }
 }
